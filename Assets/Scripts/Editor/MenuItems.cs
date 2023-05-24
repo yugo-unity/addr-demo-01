2f651a9f955341eab3dba0c7570ffd15edbd99b7
Yugo Fujioka <yugo.fujioka@unity3d.com>
2023-05-24 15:33:53 +0900

    update textures

--- Assets/Scripts/Editor/MenuItems.cs
+++ Assets/Scripts/Editor/MenuItems.cs
@@ -6,37 +6,12 @@ public static class MenuItems {
 	/// <summary>
 	/// EnemyPrefab全更新
 	/// </summary>
-	[MenuItem("Custom/Dirty Materials")]
+	[MenuItem("Custom/Verify to update Assets")]
 	public static void UpdateMaterials() {
-		var materialGUID = AssetDatabase.FindAssets("t:Material");
+		var materialGUID = AssetDatabase.FindAssets("t:Object", new[] { "Assets" });
 		foreach (var guid in materialGUID) {
 			var path = AssetDatabase.GUIDToAssetPath(guid);
-			var mat = AssetDatabase.LoadAssetAtPath<Material>(path);
-			EditorUtility.SetDirty(mat);
-		}
-	}
-	/// <summary>
-	/// EnemyPrefab全更新
-	/// </summary>
-	[MenuItem("Custom/Dirty ScriptableObject")]
-	public static void UpdateSO() {
-		var materialGUID = AssetDatabase.FindAssets("t:ScriptableObject");
-		foreach (var guid in materialGUID) {
-			var path = AssetDatabase.GUIDToAssetPath(guid);
-			var mat = AssetDatabase.LoadAssetAtPath<ScriptableObject>(path);
-			EditorUtility.SetDirty(mat);
-		}
-	}
-	/// <summary>
-	/// EnemyPrefab全更新
-	/// </summary>
-	[MenuItem("Custom/Dirty Prefabs")]
-	public static void UpdatePrefabs() {
-		var materialGUID = AssetDatabase.FindAssets("t:Prefab");
-		foreach (var guid in materialGUID) {
-			var path = AssetDatabase.GUIDToAssetPath(guid);
-			var mat = AssetDatabase.LoadAssetAtPath<GameObject>(path);
-			EditorUtility.SetDirty(mat);
+			AssetDatabase.ImportAsset(path, ImportAssetOptions.ForceUpdate);
 		}
 	}
 }
