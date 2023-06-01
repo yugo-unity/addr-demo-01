1df3368bc0ea6a6793d1d68304fb114f2a7458bb
Yugo Fujioka <yugo.fujioka@unity3d.com>
2023-06-01 10:42:37 +0900

    update to 2022.3.0f1

--- Assets/Scripts/Editor/MenuItems.cs
+++ Assets/Scripts/Editor/MenuItems.cs
@@ -1,10 +1,12 @@
-using UnityEngine;
 using UnityEditor;
 
 
 public static class MenuItems {
+	/// <summary>
+	/// 全アセットの serialized version を明示的に更新させる
+	/// </summary>
 	[MenuItem("Custom/Verify to update Assets")]
-	public static void UpdateMaterials() {
+	public static void VerifiedAllAssets() {
 		var guids = AssetDatabase.FindAssets("t:Object", new[] { "Assets" });
 		foreach (var guid in guids) {
 			var path = AssetDatabase.GUIDToAssetPath(guid);
